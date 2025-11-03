8521cd16841ca2f3648ee10641781e62a10a0514
laluxx <lorenzoaddamo10@gmail.com>
2025-11-03 15:16:17 +0100

    Solved more problems

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # clojureproblems
 
-A Clojure library designed to ... well, that part is up to you.
-
-## Usage
-
-FIXME
+Solutions for 4ever-clojure https://github.com/oxalorg/4ever-clojure
 
 ## License
 
-Copyright © 2025 FIXME
+Copyright © 2025 
 
 This program and the accompanying materials are made available under the
 terms of the Eclipse Public License 2.0 which is available at
--- src/clojureproblems/core.clj
+++ src/clojureproblems/core.clj
@@ -1,6 +1,8 @@
 (ns project.core
   (:require [clojure.string :as str]))
 
+;; You can find those problems at https://4clojure.oxal.org/
+
 ;;; Problem 1, Nothing but the Truth
 ;; Difficulty: elementary
 ;; Complete the expression so it will evaluate to true.
@@ -459,10 +461,188 @@
 ;; -> (= (__ [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))
 ;; -> (= (__ [44 33] 2) [44 44 33 33])
 
+(= ((fn [coll n] 
+   (apply concat 
+     (for [x coll] 
+       (repeat n x)))) [1 2 3] 2) '(1 1 2 2 3 3))
 
-(= ((fn [coll n] ()) [1 2 3] 2) '(1 1 2 2 3 3))
-(= (__ [:a :b] 4) '(:a :a :a :a :b :b :b :b))
-(= (__ [4 5 6] 1) '(4 5 6))
-(= (__ [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))
-(= (__ [44 33] 2) [44 44 33 33])
+(= ((fn [coll n] 
+   (apply concat 
+     (for [x coll] 
+       (repeat n x)))) [:a :b] 4) '(:a :a :a :a :b :b :b :b))
+
+(= ((fn [coll n] 
+   (apply concat 
+     (for [x coll] 
+       (repeat n x)))) [4 5 6] 1) '(4 5 6))
+
+(= ((fn [coll n] 
+   (apply concat 
+     (for [x coll] 
+       (repeat n x)))) [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))
+
+(= ((fn [coll n] 
+   (apply concat 
+     (for [x coll] 
+       (repeat n x)))) [44 33] 2) [44 44 33 33])
+
+;;; Problem 34, Implement range
+;; Difficulty: easy
+;; Write a function which creates a list of all integers in a given range.
+;; -> (= (__ 1 4) '(1 2 3))
+;; -> (= (__ -2 2) '(-2 -1 0 1))
+;; -> (= (__ 5 8) '(5 6 7))
+;; Special Restrictions : range
+
+(= ((fn [a b] (take (- b a) (iterate inc a))) 1 4) '(1 2 3))
+(= ((fn [a b] (take (- b a) (iterate inc a))) -2 2) '(-2 -1 0 1))
+(= ((fn [a b] (take (- b a) (iterate inc a))) 5 8) '(5 6 7))
+
+
+;;; Problem 35, Local bindings
+;; Difficulty: elementary
+;; Clojure lets you give local names to values using the special let-form.
+;; -> (= __ (let [x 5] (+ 2 x)))
+;; -> (= __ (let [x 3, y 10] (- y x)))
+;; -> (= __ (let [x 21] (let [y 3] (/ x y))))
+
+(= 7 (let [x 5] (+ 2 x)))
+(= 7 (let [x 3, y 10] (- y x)))
+(= 7 (let [x 21] (let [y 3] (/ x y))))
+
+
+;;; Problem 36, Let it Be
+;; Difficulty: elementary
+;; Can you bind x, y, and z so that these are all true?
+;; -> (= 10 (let __ (+ x y)))
+;; -> (= 4 (let __ (+ y z)))
+;; -> (= 1 (let __ z))
+
+
+(= 10 (let [[x y z][7 3 1]] (+ x y)))
+(= 4 (let  [[x y z][7 3 1]] (+ y z)))
+(= 1 (let  [[x y z][7 3 1]] z))
+
+
+;;; Problem 37, Regular Expressions
+;; Difficulty: elementary
+;; Regex patterns are supported with a special reader macro.
+;; -> (= __ (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
+
+(= "ABC" (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
+
+
+;;; Problem 38, Maximum value
+;; Difficulty: easy
+;; Write a function which takes a variable number of parameters and returns the maximum value.
+;; -> (= (__ 1 8 3 4) 8)
+;; -> (= (__ 30 20) 30)
+;; -> (= (__ 45 67 11) 67)
+;; Special Restrictions : max,max-key
+
+
+(= ((fn [& nums]
+      (last (sort nums))) 1 8 3 4) 8)
+
+(= ((fn [& nums]
+      (last (sort nums))) 30 20) 30)
+
+(= ((fn [& nums]
+      (last (sort nums))) 45 67 11) 67)
+
+;;; Problem 39, Interleave Two Seqs
+;; Difficulty: easy
+;; Write a function which takes two sequences and returns the first item from each, then the second item from each, then the third, etc.
+;; -> (= (__ [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
+;; -> (= (__ [1 2] [3 4 5 6]) '(1 3 2 4))
+;; -> (= (__ [1 2 3 4] [5]) [1 5])
+;; -> (= (__ [30 20] [25 15]) [30 25 20 15])
+
+
+(= ((fn [coll1 coll2]
+      (mapcat list coll1 coll2)) [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
+
+(= ((fn [coll1 coll2]
+      (mapcat list coll1 coll2)) [1 2] [3 4 5 6]) '(1 3 2 4))
+
+(= ((fn [coll1 coll2]
+      (mapcat list coll1 coll2)) [1 2 3 4] [5]) [1 5])
+
+(= ((fn [coll1 coll2]
+      (mapcat list coll1 coll2)) [30 20] [25 15]) [30 25 20 15])
+
+
+;;; Problem 40, Interpose a Seq
+;; Difficulty: easy
+;; Write a function which separates the items of a sequence by an arbitrary value.
+;; -> (= (__ 0 [1 2 3]) [1 0 2 0 3])
+;; -> (= (apply str (__ ", " ["one" "two" "three"])) "one, two, three")
+;; -> (= (__ :z [:a :b :c :d]) [:a :z :b :z :c :z :d])
+;; Special Restrictions : interpose
+
+(= ((fn [sep coll]
+      (butlast (mapcat #(vector % sep) coll))) 0 [1 2 3]) [1 0 2 0 3])
+
+(= (apply str ((fn [sep coll]
+      (butlast (mapcat #(vector % sep) coll))) ", " ["one" "two" "three"])) "one, two, three")
+
+(= ((fn [sep coll]
+      (butlast (mapcat #(vector % sep) coll))) :z [:a :b :c :d]) [:a :z :b :z :c :z :d])
+
+
+;;; Problem 41, Drop Every Nth Item
+;; Difficulty: easy
+;; Write a function which drops every Nth item from a sequence.
+;; -> (= (__ [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8])
+;; -> (= (__ [:a :b :c :d :e :f] 2) [:a :c :e])
+;; -> (= (__ [1 2 3 4 5 6] 4) [1 2 3 5 6])
+
+(= ((fn [coll n]
+  (mapcat #(take (dec n) %) (partition-all n coll))) [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8])
+
+(= ((fn [coll n]
+  (mapcat #(take (dec n) %) (partition-all n coll))) [:a :b :c :d :e :f] 2) [:a :c :e])
+
+(= ((fn [coll n]
+  (mapcat #(take (dec n) %) (partition-all n coll))) [1 2 3 4 5 6] 4) [1 2 3 5 6])
+
+
+;;; Problem 42, Factorial Fun
+;; Difficulty: easy
+;; Write a function which calculates factorials.
+;; -> (= (__ 1) 1)
+;; -> (= (__ 3) 6)
+;; -> (= (__ 5) 120)
+;; -> (= (__ 8) 40320)
+
+(= ((fn factorial [n]
+      (if (<= n 1)
+        1
+        (* n (factorial (dec n))))) 1) 1)
+
+(= ((fn factorial [n]
+      (if (<= n 1)
+        1
+        (* n (factorial (dec n))))) 3) 6)
+
+(= ((fn factorial [n]
+      (if (<= n 1)
+        1
+        (* n (factorial (dec n))))) 5) 120)
+
+(= ((fn factorial [n]
+      (if (<= n 1)
+        1
+        (* n (factorial (dec n))))) 8) 40320)
+
+
+
+;;; Problem 43, Reverse Interleave
+;; Difficulty: medium
+;; Write a function which reverses the interleave process into x number of subsequences.
+;; -> (= (__ [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
+;; -> (= (__ (range 9) 3) '((0 3 6) (1 4 7) (2 5 8)))
+;; -> (= (__ (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))
+
+
 
